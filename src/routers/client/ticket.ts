5f77c36aa45decdbf2a2224bd3717885e09f17a9
Ben Hur Faria <benhurfaria3@gmail.com>
2022-01-24 01:36:03 -0300

    Add ticket interface and post

--- src/routers/client/ticket.ts
+++ src/routers/client/ticket.ts
@@ -5,5 +5,6 @@ import * as controller from "@/controllers/client/ticket";
 const router = Router();
 
 router.get("/", controller.getTicketInfos);
+router.post("/", controller.postTicketInfos);
 
 export default router;
--- src/services/client/ticket.ts
+++ src/services/client/ticket.ts
@@ -1,5 +1,10 @@
 import Ticket from "../../entities/Ticket";
+import TicketInterface from "@/interfaces/ticket";
 
 export async function getTicketInfos(userId: number) {
   return await Ticket.getTicketInfos(userId);
+}
+
+export async function postTicketInfos(ticket: TicketInterface) {
+  await Ticket.postTicketInfos(ticket);
 }
