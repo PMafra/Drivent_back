28bd3467d71c273dd526006df13a681ac2ad8a9c
Ben Hur Faria <benhurfaria3@gmail.com>
2022-01-31 01:09:28 -0300

    static activity function

--- src/entities/Activity.ts
+++ src/entities/Activity.ts
@@ -1,4 +1,4 @@
-import { 
+import {
   BaseEntity,
   Entity,
   PrimaryGeneratedColumn,
@@ -10,6 +10,7 @@ import {
 import EventDay from "./EventDay";
 import Hall from "./Hall";
 import Subscription from "./Subscription";
+import NotValidEntity from "@/errors/NotValidEntity";
 
 @Entity("activities")
 export default class Activity extends BaseEntity {
@@ -34,24 +35,48 @@ export default class Activity extends BaseEntity {
   @Column()
   hallId: number;
 
-  @ManyToOne(() => EventDay, (eventDay: EventDay) => (eventDay.activities), {
+  @ManyToOne(() => EventDay, (eventDay: EventDay) => eventDay.activities, {
     eager: true,
     nullable: false,
   })
   @JoinColumn({ name: "eventDayId" })
-    eventDay: EventDay;
+  eventDay: EventDay;
 
-  @ManyToOne(() => Hall, (hall: Hall) => (hall.activities), {
+  @ManyToOne(() => Hall, (hall: Hall) => hall.activities, {
     eager: true,
     nullable: false,
   })
   @JoinColumn({ name: "hallId" })
-    hall: Hall;
+  hall: Hall;
 
   @OneToMany(() => Subscription, (subscription) => subscription.activity)
-    subscriptions: Subscription[];
+  subscriptions: Subscription[];
 
   static async getEventDayActivities(eventDayId: number) {
     return await this.find({ where: { eventDayId } });
+  }
+
+  static checkTimingConflict(allActivities: Subscription[], activity: Activity) {
+    return;
+  }
+
+  static async postSubscribe(ticketId: number, activityId: number) {
+    const activity = await this.findOne({
+      where: { id: activityId },
+    });
+
+    const userTicket = await Subscription.findOne({
+      where: { ticketId, activityId },
+    });
+
+    if (!activity) throw new NotValidEntity("A atividade não existe");
+    if (activity.totalSeats === 0) throw new NotValidEntity("Atividade lotada");
+    if (userTicket) throw new NotValidEntity("Usuário ja cadastrado");
+
+    const allActivities = await Subscription.find({
+      where: { ticketId },
+      relations: ["activities"],
+    });
+    this.checkTimingConflict(allActivities, activity);
   }
 }
